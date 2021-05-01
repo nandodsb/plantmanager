26cc002c2267760e4e61218718844ddfd5777a15
Fernando <nandosbx@users.noreply.github.com>
2021-05-01 11:59:59 -0300

    Config Json-Server Json

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import * as dotenv from 'dotenv'
-dotenv.config()
 
 import StackRoutes from './stack.routes'
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import endpoint from '../config/endpoints.config';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.6:3333'
+  baseURL: endpoint.BASE_URL
 })
 
 export default api
